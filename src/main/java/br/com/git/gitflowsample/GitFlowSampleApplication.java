e89f949570b055b552a8bec15fdf8ec1461eab10
Diego Pangone <diegopangone@gmail.com>
2021-07-14 11:24:13 -0300

    Correção do id inicial do título

--- src/main/java/br/com/git/gitflowsample/GitFlowSampleApplication.java
+++ src/main/java/br/com/git/gitflowsample/GitFlowSampleApplication.java
@@ -8,6 +8,8 @@ public class GitFlowSampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitFlowSampleApplication.class, args);
+
+        System.out.println("Teste programa");
     }
 
 }
